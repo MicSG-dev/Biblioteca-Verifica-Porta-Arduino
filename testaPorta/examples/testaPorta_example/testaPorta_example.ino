e676b9a1da2cf117b6bf2ff935e37ca818643f71
MichelSiG <71986598+MicSG-dev@users.noreply.github.com>
2021-02-16 20:50:43 -0300

    Update testaPorta_example.ino

--- testaPorta/examples/testaPorta_example/testaPorta_example.ino
+++ testaPorta/examples/testaPorta_example/testaPorta_example.ino
@@ -3,20 +3,24 @@
 
 void setup() {
   // put your setup code here, to run once:
-  //Serial.begin(9600);
+  Serial.begin(9600);
   testePorta led(13);
-  if(!led.iniciaPortaSaida()){
-    Serial.println("\nERRO de Inicialização.");
-    while(1){
-      
+  if (!led.iniciaPortaSaida()) {
+    Serial.println("ERRO de Inicialização.");
+    while (1) {
+
     }
+  } else {
+    Serial.println("Porta 13 inicializada com sucesso");
   }
-  
+
+  Serial.println("Sem erro, pois foi feito com outra serial");
+
 
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+
 }
