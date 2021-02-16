f06036ac9e6ffeed47377a706a03a4f736e87b8b
MichelSiG <71986598+MicSG-dev@users.noreply.github.com>
2021-02-16 20:51:46 -0300

    Update testePorta.cpp

--- testaPorta/testePorta.cpp
+++ testaPorta/testePorta.cpp
@@ -9,32 +9,20 @@ testePorta::testePorta(int pin)
 }
 
 bool testePorta::iniciaPortaSaida() {
-  if(!Serial){
-    Serial.begin(9600);
-	Serial.print("Serial iniciada");
-  }
     while (controleWhile) {
       pinMode(_pin, OUTPUT);
-      Serial.print("Iniciando Teste de Porta Entrada ");
-      Serial.println(_pin);
       delay(5000);
       digitalWrite(_pin, HIGH);
 
       if (digitalRead(_pin) != HIGH) {
-        Serial.println("Porta não iniciado\n");
         delay(500);
         i++;
       } else {
-        Serial.println("Porta OK");
-        delay(1000);
         controleWhile = false;
         digitalWrite(_pin, LOW);
         retorno = true;
       }
       if (i > 3) {
-        Serial.println("Não foi possível iniciar a Porta");
-        delay(1000);
-        Serial.println("Verifique as conexões. Após verificar as conexões, aperte Reset na placa");
         controleWhile = false;
         retorno = false;
       }
